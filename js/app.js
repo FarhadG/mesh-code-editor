7d1e6b610f4ecdfad26af7e75d9361065f22993e
Farhad Ghayour <me@farhadg.com>
2013-12-12 23:59:52 +0800

    javascript and jQuery injected

--- js/app.js
+++ js/app.js
@@ -1,9 +1,9 @@
 $(function() {
 
   $('#preview').height($(window).height());
-  $('div.CodeMirror').click(function() {
-    $(this).css('height', $(window).height()); 
-  });
+  // $('div.CodeMirror').click(function() {
+  //   $(this).css('height', $(window).height()); 
+  // });
 
   // $textarea.click(function() {
   //   var $this = $(this);
@@ -34,33 +34,15 @@ $(function() {
   //   }
   // });
 
-  // $textarea.keyup(function() {
-  //   var htmlContent = $('textarea.html').val();
-  //   var cssContent  = $('textarea.css').val();
-  //   var jsContent   = $('textarea.js').val();
-
-  //   $("#frame").attr(
-  //     "src", "data:text/html;charset=utf-8," + 
-  //     "<html>" +  
-  //     "<style>" +
-  //     cssContent +
-  //     "</style>" +
-  //     htmlContent +
-  //     "<script src=\"http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js\"><" + "/script>" +    
-  //     "<script>" + 
-  //     jsContent + 
-  //     "<" +
-  //     "/script>" +         
-  //     "</html>"
-  //   );
-  // });
-
+  
+  
 
   var html = CodeMirror.fromTextArea(document.getElementById("html"), {
     theme: "monokai",
     lineNumbers: true,
     mode: 'xml',
-    htmlMode: true
+    htmlMode: true,
+    defaultValue: "cool"
   });
 
   var css = CodeMirror.fromTextArea(document.getElementById("css"), {
@@ -75,21 +57,39 @@ $(function() {
     mode: "text/javascript"
   });
 
-  var code = html.getValue();
+  var htmlContent = html.getValue();
+  var cssContent = css.getValue();
+  var jsContent = js.getValue();
+
+  var code = function() {
+    return "<html>" +  
+      "<style>" +
+      cssContent +
+      "</style>" +
+      htmlContent +
+      "<script src=\"http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js\"><" + "/script>" +    
+      "<script>" + 
+      jsContent + 
+      "<" +
+      "/script>" +         
+      "</html>";
+  };
 
   var delay;
   html.on("change", function() {
-    code = html.getValue();
+    htmlContent = html.getValue();
     clearTimeout(delay);
     delay = setTimeout(updatePreview, 300);
   });
 
   css.on("change", function() {
+    cssContent = css.getValue();
     clearTimeout(delay);
     delay = setTimeout(updatePreview, 300);
   });
 
   js.on("change", function() {
+    jsContent = js.getValue();
     clearTimeout(delay);
     delay = setTimeout(updatePreview, 300);
   });
@@ -98,7 +98,7 @@ $(function() {
     var previewFrame = document.getElementById('preview');
     var preview =  previewFrame.contentDocument ||  previewFrame.contentWindow.document;
     preview.open();
-    preview.write(code);
+    preview.write(code());
     preview.close();
   }
   setTimeout(updatePreview, 300);
