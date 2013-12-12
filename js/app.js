90b723e9359f960238b6e72fda0240f291e8c374
Farhad Ghayour <me@farhadg.com>
2013-12-12 22:58:14 +0800

    html working with preview

--- js/app.js
+++ js/app.js
@@ -1,11 +1,42 @@
 $(function() {
+  var html = CodeMirror.fromTextArea(document.getElementById("html"), {
+    theme: "monokai",
+    lineNumbers: true,
+    mode: 'xml',
+    htmlMode: true
+  });
+
+  var css = CodeMirror.fromTextArea(document.getElementById("css"), {
+    theme: "monokai",
+    lineNumbers: true,
+    indentUnit: 2
+  });
+
+  var js = CodeMirror.fromTextArea(document.getElementById("js"), {
+    theme: "monokai",
+    lineNumbers: true,
+    mode: "text/javascript"
+  });
+
+  // event listeners
+
   var delay;
   html.on("change", function() {
     clearTimeout(delay);
     delay = setTimeout(updatePreview, 300);
   });
+
+  css.on("change", function() {
+    clearTimeout(delay);
+    delay = setTimeout(updatePreview, 300);
+  });
+
+  js.on("change", function() {
+    clearTimeout(delay);
+    delay = setTimeout(updatePreview, 300);
+  });
   
-  function updatePreview() {
+  function updatePreview(el) {
     var previewFrame = document.getElementById('preview');
     var preview =  previewFrame.contentDocument ||  previewFrame.contentWindow.document;
     preview.open();
